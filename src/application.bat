1a8217fd963c25e48c8b37b6f587a1b0033f6a53
Vincent Tienvrot <v.tienvrot@iia-laval.fr>
2018-10-09 14:29:09 +0200

    add var to script

--- src/application.bat
+++ src/application.bat
@@ -1,4 +1,6 @@
 ###application.bat - 2018 - Tienvrot###
 echo "Copyright company - 2018"
-echo "version 1.1"
-echo "Build : 10:00"
+version="2"
+build_hour="11:00"
+echo "Build ${build_hour}"
+echo "version ${version}"
